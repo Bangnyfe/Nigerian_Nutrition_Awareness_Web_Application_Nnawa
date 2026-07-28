7c5d5422bd7c20e8ec510ae545ca82ddcc68d72d
A-DAIB <a.daib@alustudent.com>
2026-07-28 12:41:10 +0100

    Add CRUD operations for products in routes

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import {
   getProducts,
-  getProductById
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct
 } from '../controllers/productController.js';
 
 const router = Router();
 
 router.get('/', getProducts);
+router.post('/', createProduct);
 router.get('/:id', getProductById);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 export default router;
