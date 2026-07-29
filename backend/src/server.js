ef58fc1167394d5ab33ebb91bbf01575faa31cd0
A-DAIB <a.daib@alustudent.com>
2026-07-29 22:03:11 +0100

    Initialize admin user on server start
    Added bootstrapAdmin function to initialize admin user on first run.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
 import app from './app.js';
 import { env, validateEnv } from './config/env.js';
 import { initDatabase } from './database/initDatabase.js';
+import { bootstrapAdmin } from './database/bootstrapAdmin.js';
 
 function startServer() {
   try {
     validateEnv();
     initDatabase();
+
+    // First-run only: creates the administrator if none exists yet.
+    bootstrapAdmin();
 
     app.listen(env.port, () => {
       console.log(
@@ -17,5 +21,4 @@ function startServer() {
     process.exit(1);
   }
 }
-
 startServer();
